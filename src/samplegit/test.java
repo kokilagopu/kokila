d3c7cba4116bfe183ca8d09f3d85f820873f7439
kokila <kokiladev26@gmail.com>
2022-08-17 20:30:22 +0530

    new code added

---
+++ src/samplegit/test.java
@@ -0,0 +1,16 @@
+package samplegit;
+
+public class test {
+
+	public static void main(String[] args) {
+String hungry="yes";
+if (hungry=="yes")
+System.out.println("eat something");
+else {
+	System.out.println(" dont eat something");
+
+}
+
+	}
+
+}
